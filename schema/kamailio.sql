5f01cb0bbc36fc8e80dc8695846a0b843fd8c1b0
Sipwise Jenkins Builder <jenkins@sipwise.com>
2023-03-29 13:13:10 +0200

    MT#55307 fix not replicated dbscript name, schemes update
    Change-Id: I0be0d940b752dfd1401b1258db98d2ca8f059d5c

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -790,7 +790,7 @@ CREATE TABLE `subscriber` (
   KEY `uuid_idx` (`uuid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','8353ab8f32ac02c25b81aac4564cf2da','000ff269816884209a318b5ba99d683a','d6ad8fa82767629149db6e87f9899cc9','9bcb88b6-541a-43da-8fdc-816f5557ff93','',NOW());
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','109892255fd17e33ac74f088df4a334b','7b997d5e3faf5742b374a75a5b0b1b77','0c88c1f7cbf54f4a251a495323cffc54','9bcb88b6-541a-43da-8fdc-816f5557ff93','',NOW());
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `trusted` (
