b5154890b1601f6da5c29990a3263d3af76b30a3
Sipwise Jenkins Builder <jenkins@sipwise.com>
2023-07-14 11:47:14 +0200

    MT#56845 New subscriber preference: `cloud_pbx_hunt_cancel_mode`, schemes update
    Change-Id: Icb774c5d4b0c16a9aa8812d05fc3c4f38c0b1e98

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1069,7 +1069,7 @@ INSERT INTO `dom_preferences` VALUES (27,'','0','voip.sipwise.local','play_annou
 INSERT INTO `dom_preferences` VALUES (28,'','0','voip.sipwise.local','mobile_push_enable',0,'never',NOW());
 INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_member_mode',0,'ring',NOW());
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local',NOW(),NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','231501f187a03194d714cfd6d7864656','de88af620556a96ead8284902c8e26c2','327520841144ed8f1b326fe8be3e2afd','9bcb88b6-541a-43da-8fdc-816f5557ff93','',NOW());
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','0676c2e016a3b2215c045ae25f81df38','f34bbbf613348b274fd24d8836653356','1c9d1bd99fdba88235f19a8251a29552','9bcb88b6-541a-43da-8fdc-816f5557ff93','',NOW());
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none',NOW());
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack',NOW());
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never',NOW());
