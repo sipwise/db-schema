21861da9f5b30cbbc01f88097e7b0a4302b5c00f
Sipwise Jenkins Builder <jenkins@sipwise.com>
2023-10-17 17:09:09 +0000

    MT#57598 Add explicit_defaults_for_timestamp option, schema update
    Change-Id: I8f54d9806e5623803fd50dd8faf0b8601a15c4ca

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1069,7 +1069,7 @@ INSERT INTO `dom_preferences` VALUES (27,'','0','voip.sipwise.local','play_annou
 INSERT INTO `dom_preferences` VALUES (28,'','0','voip.sipwise.local','mobile_push_enable',0,'never','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_member_mode',0,'ring','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','6001036beeecabc92d044652b2bfa793','cb76265a8ee522b15d96cf9bd4341d2a','c53f5b5e7e8d1d0b71c4cd165a7d8de3','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','0182d314a021ffccd2b1d42544a9e2cc','f6f2143ac4d76732e15670d2bce9e75b','67a75e3cbd146ed6611b00fab196632c','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
