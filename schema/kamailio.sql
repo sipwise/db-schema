3db4adfebd077af8bb79ebbd35ae8d3d96787b36
Sipwise Jenkins Builder <jenkins@sipwise.com>
2023-11-17 15:46:05 +0000

    MT#58768 fix voip_allowed_ip_groups.ipnet column, schemes update
    Change-Id: I8a8dc1b810f1c7918baa8b58fd669ed205a38932

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1071,7 +1071,7 @@ INSERT INTO `dom_preferences` VALUES (27,'','0','voip.sipwise.local','play_annou
 INSERT INTO `dom_preferences` VALUES (28,'','0','voip.sipwise.local','mobile_push_enable',0,'never','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_member_mode',0,'ring','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','9b8c3e6e3fc934be0971f6e3eca219ce','ab11d0907351f20ebeba6a7465c35e05','978bb860e9577128e51083254521e003','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','671fd0378585eca7a7730adfa6b2c293','3c9dba1ffb1d12025dd32fdd80928a9c','0bfc888079a778740183f5f100040d83','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
