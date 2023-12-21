8c5d9c0ea3c2e46d9645aad3e7095fe92db849b2
Rene Krenn <rkrenn@sipwise.com>
2023-12-21 16:43:03 +0100

    MT#58629 persist presentity body 12.1
    Change-Id: Iebd12908d7ca845b00eb26bc66b25f385a32d197

--- schema/accounting.sql
+++ schema/accounting.sql
@@ -298,6 +298,17 @@ CREATE TABLE `cdr_period_costs` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cdr_presentity` (
+  `call_id` varchar(255) NOT NULL,
+  `event` varchar(64) NOT NULL,
+  `received_time` decimal(13,3) NOT NULL,
+  `body` text NOT NULL,
+  PRIMARY KEY (`call_id`,`event`,`received_time`),
+  KEY `cdr_presentity_rtime_idx` (`received_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cdr_provider` (
   `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `type` enum('carrier','reseller','customer') NOT NULL,
