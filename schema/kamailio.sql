e2cb6cb7945f08455ca5da8eeaa38941d30ffc6c
Kirill Solomko <ksolomko@sipwise.com>
2024-01-24 13:34:27 +0100

    MT#58709 address update_sound_set_handle_parents() sp deadlocks
    * update_sound_set_handle_parents() procedure is redesigned
      to avoid "insert into select from" statements as they cause
      gap locks when run concurrently and access the same index
      rows.
    
    *** WAITING FOR THIS LOCK TO BE GRANTED:
    RECORD LOCKS space id 6594 page no 6 n bits 56 index parent_id_idx
    of table `provisioning`.`voip_sound_sets` trx id 400852
    lock mode S locks rec but not gap waiting
    
      the "insert into select from" statements are reworked:
       the "select from/with recursive" parts are now declared cursors
       and the data is inserted inside the opened cursor loop (if any).
       this way the selects are not causing exclusive gap locks.
    
    (cherry picked from commit 4017d3519cc9f49db153ae09de66d96a782a8b32)
    (cherry picked from commit 9c0fd6e4a1889ddbcbb3a90521dd3e2d3bfe73a5)
    
    Change-Id: I35ec28a33ecca42de898e67a2709eda4364e5f95

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1072,7 +1072,7 @@ INSERT INTO `dom_preferences` VALUES (28,'','0','voip.sipwise.local','mobile_pus
 INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_member_mode',0,'ring','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','320eca022b522e8e009d35397ccf3375','517e482cbffa58b026b862ecf3416df0','3d76cb99cd27de05055bd92030b1ea35','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a1d2bedb49a2324f7ed170160330d518','22bc890013582089d0d3555bf1b59df4','1c9afab3fbdd55180f731d4fac01b42e','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
