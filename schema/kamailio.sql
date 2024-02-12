d7e92ea5cb132353e7a7b00815c80f548c3af8cc
Kirill Solomko <ksolomko@sipwise.com>
2024-02-12 22:31:23 +0100

    MT#59386 address foreign key handling in 6425.up/down
    * Instead of using "set foreign_key_checks=0" before
      altering voicemail_spool voicemail_users tables the
      foreign key voicemail_spool.`v_s_mailboxuser_ref` is
      dropped instead and re-created after the alter table operations
      (same lines where "set foreign_key_checks=0" and "set
      foreign_key_checks=1" were respectively.
    * That addresses the changed behavior with MariaDB 10.11.6
      where "alter table" statements the change columns involved
      in a foreign key and potentially causing data loss are prohibited
      even with "set foreign_key_checks=0".
    * This change is only valid for new deployments and databases
      with already applied 6425.up are not affected.
    
    (cherry picked from commit 25257f1657b3675106fccd45842e71602e839a8c)
    
    Change-Id: I2f2a85ca9f9f7d15058f41f5d8eaaec23ec501fa

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1072,7 +1072,7 @@ INSERT INTO `dom_preferences` VALUES (28,'','0','voip.sipwise.local','mobile_pus
 INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_member_mode',0,'ring','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a1d2bedb49a2324f7ed170160330d518','22bc890013582089d0d3555bf1b59df4','1c9afab3fbdd55180f731d4fac01b42e','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','41a0935a5c4a2abfe2ab374887a57492','1e2b62a9beb5d43c9dbbe55ad134c1ec','9c8fd2fe1d6ec623b71c87906bb67f77','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
