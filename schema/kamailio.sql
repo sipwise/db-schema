60f955429c891da4fd72f9b08acd8d5e42bcf55e
Donat Zenichev <dzenichev@sipwise.com>
2024-02-23 11:15:54 +0100

    MT#59482 Intoduce new database `freeswitch`
    Introduce new database `freeswitch` with according
    tables and views:
    - `aliases`
    - `basic_calls` (view)
    - `calls`
    - `channels`
    - `complete`
    - `detailed_calls` (view)
    - `interfaces`
    - `nat`
    - `registrations`
    - `recovery`
    - `tasks`
    - `sip_subscriptions`
    - `sip_dialogs`
    - `sip_presence`
    - `sip_authentication`
    - `sip_shared_appearance_subscriptions`
    - `sip_shared_appearance_dialogs`
    
    Change-Id: Iedb3c628c83db2cd5acb2a9ec67eecad626020d5

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','b372c4bbef38aad1c3aa3a9ef3f70195','35139db822246e3fa031cb15f2940f60','cb60f4c9225901694760ca00681c96f7','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','7341e79192ce090c2ecdf5677d136b7e','7cdab583017c2213d37a381d6a7347a7','fb58f193b3aabdd8e941643256279601','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
