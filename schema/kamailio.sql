66174a66a222c1c7cdba6fb308c9ef9a870e818f
Kirill Solomko <ksolomko@sipwise.com>
2024-03-03 13:41:54 +0100

    MT#59348 peering server create trigger fix preferences
    * voip_phost_crepl_trig: enum default preferences are now always
      created regardless of the enabled state, otherwise that led
      to peering servers to be created without their default
      not null enum preferences in voip_peer_preferences.
    * fix missing not null enum preferences for peering servers
    
    Change-Id: If8cecb5570ba876643207931ecab1e42a994a2fe

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','1cbcb383dff5c2dacc9cdd3a53645bd5','a658443dbd5d4695a854d71c5d79e091','2cdc45a706b1f7a1e786fecb8f59690e','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','0a7e018f26e4b421e6333dc4919fd01c','abf7efc87e29eb2e0698ed4d35d3db04','14266f65d193aef56eca8f88aace3742','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
