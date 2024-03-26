4df94368f832b654863397fd5b0257d0de741ee4
Marco Capetta <mcapetta@sipwise.com>
2024-03-26 17:06:45 +0100

    MT#59749 Add new column 'use_redirection' to 'provisioning.cf_mappings'
    This new column will be used to store a flag (value 0 or 1) that
    controls wether the CF is processed as usual (flag with value 0,
    default) or generates a 302 redirect message back to the
    caller (flag with value 1).
    
    Updating also the 'v_subscriber_cfs' VIEW to add thhis new column.
    
    Change-Id: I74e53a526810f0145e305b90c3c3bd56cf91bc61

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','4ef99b65859123a0721fd40f79675cc9','2374a9315c518aba8cd5eefca10ffe1e','175681af9ef05a345bc81a3bf9261487','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','182043159b9c759680bbd285590fd42f','16cb6eb4585ccbd29703d2fa965a009b','81a0450d1727e1297336dae2ea729325','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
