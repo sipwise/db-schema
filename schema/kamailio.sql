056e857866f13f764d1039834f7c1a52a39e9b84
Kirill Solomko <ksolomko@sipwise.com>
2024-04-29 20:58:49 +0200

    MT#59988 fix update-db-schema ngcp.db_schema locks
    * get_db_schema() now issues a commit after selects to cleanup
      any pending ngcp.db_schema row locks as otherwise applying
      an .up script that modifies ngcp.db_schema (usually applied outside
      of the connection handle by invoking 'mysql' command) causes
      wait locks.
    
    (cherry picked from commit 73dcba900fa82828d98b1e24247853c7502e67f6)
    
    Change-Id: Ie0557a1a7648baeba2ec6ded3665c51667877101

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','3fb510f1832d859d8839f6201451e20c','c9f4a61bc64c33f1b3c5080303938cb8','6b6bf813a71f8c00154d2f77005ee78e','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a37af98d27bab5e638964f8a093de71a','49b2aafddb24530f7535fc2eda343570','470df66c86cb49a42234a21fd3e13a49','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
