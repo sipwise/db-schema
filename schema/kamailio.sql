ea32e2471eb9113b8896229034b78ca5b929f158
Marco Capetta <mcapetta@sipwise.com>
2024-05-07 15:06:24 +0200

    MT#59736 Fix addition of 'rcv_display/np_display' in 'outbound_X_display'
    By mistake in commit 7aab6d290d7 the new 'rcv_display/np_display'
    option was added to the 'outbound_from_user' and 'outbound_pai_user'
    preferences instead of 'outbound_from_display' and 'outbound_pai_display'.
    
    This fix revert previous addition and add it to the correct preference.
    
    (cherry picked from commit bff02d29c9748c6c0378c554e41dd7cfcdf1cad4)
    
    Change-Id: Ifed659b22dbde621fbf366092268e6416e9ad4ba

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a37af98d27bab5e638964f8a093de71a','49b2aafddb24530f7535fc2eda343570','470df66c86cb49a42234a21fd3e13a49','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','2d79ee154b034d0e0dc2088b5d0580ca','f5f339fdd311510f19c2092296e6f5c7','c27d3e7e7ac9a3d56a1a20ce5c22d942','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
