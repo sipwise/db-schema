aa220accb137f383e29f7e117d384d592a43cea5
Marco Capetta <mcapetta@sipwise.com>
2024-05-09 15:40:54 +0200

    MT#59736 Fix addition of 'rcv_display/np_display' in 'outbound_X_display'
    By mistake in commit 7aab6d290d7 the new 'rcv_display/np_display'
    option was added to the 'outbound_from_user' and 'outbound_pai_user'
    preferences instead of 'outbound_from_display' and 'outbound_pai_display'.
    
    This fix revert previous addition and add it to the correct preference.
    
    (cherry picked from commit bff02d29c9748c6c0378c554e41dd7cfcdf1cad4)
    (cherry picked from commit ea32e2471eb9113b8896229034b78ca5b929f158)
    
    Change-Id: Ifed659b22dbde621fbf366092268e6416e9ad4ba

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','3fb510f1832d859d8839f6201451e20c','c9f4a61bc64c33f1b3c5080303938cb8','6b6bf813a71f8c00154d2f77005ee78e','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','47c89d864050aab4dab5b08d3004339c','853009d29d2603a0f50e503c547156e5','0d9f604740a5de71e73c205eccc8eaa0','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
