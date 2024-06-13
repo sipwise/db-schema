02dbc98c67a1170187884e53f13139d26e828699
Alessio Garzi <agarzi@sipwise.com>
2024-06-13 10:12:07 +0200

    MT#60309 Enable pref ignore_cf_when_hunting only at customer level
    Kamailio proxy evaluates the preference "ignore_cf_when_hunting" only at customer
    (contract) level but web interface lets the user set this pref also at domain
    and subscriber level.
    This leads to some confusion, the web interface user is fooled by this preference
    and thinks something is wrong inside kamailio proxy because enabling/disabling
    "ignore_cf_when_hunting" at dom/subscriber level has no effect at all.
    Original implementation treats this option as something that must have
    a global impact as demostrated by the same preference into config.yml.
    
    (cherry picked from commit 5f0bfdae7c300871a6e3d4f6eede7f7bdf1a358b)
    
    Change-Id: I2ba28709dac8a3348049ad9dd5f116ce9d86d9f8

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','2c84ca5ef716a8b8b350b01ea5d64c23','611ccf05f064dbf479f9e55b1ac4a81a','4b6cdb5b93834f7c1fa5e783d7a28369','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','7a9a4c7690865807cdf27a2458cc4cef','891a83a23e260e7e3b3f9c35c715335e','95167011dbe3c11dfda3d90796fcd5cd','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
