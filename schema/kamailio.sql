cde661b8faff8c79272d2c9add87d4d42421453b
Kirill Solomko <ksolomko@sipwise.com>
2024-07-08 11:22:15 +0200

    MT#60468 fix undefiend force_ro_db_sync error
    * use getattr to correctly fetch the Config value
    
    (cherry picked from commit fc1b59ab13fc9264079ed3fd98a875f24ffd4e4c)
    
    (cherry picked from commit 8a985e2ae5949f5b47e59769d3045eadab1706ea)
    
    Change-Id: I84224c730d07efa466ecdd135c5eb6e09afe617b

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','c9f4279ad0841a6fe612cd98d609600e','f6ae4d0db2303d53223769a1bc254cf7','4de14cd383ac63b464ce1170a7a0eb53','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','9168b68cd7091be2e40d22483e891c37','0b24c5a6f36e9602dd62312221116709','2abf25394c65bf3b2eb75f09683a0fc5','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
