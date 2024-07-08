9257b657561bd4692b66ef7474d7d19f3899ac45
Donat Zenichev <dzenichev@sipwise.com>
2024-07-08 13:08:18 +0200

    MT#58625 Fixup for 8a4082e20
    Use a real NULL value instead of string.
    
    Additionally: update description for the preference
    'announce_conn_type'.
    
    (cherry picked from commit 7932f83a788dc042e4ef47ddebc922a92545616d)
    (cherry picked from commit a7eb376fe801d5efe93984c267f17bf8112984be)
    
    Change-Id: I144702d0b6decfde42b075eb50399b68ed5b4818

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,11 +1161,11 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','5196051024d7f7545077b8408b8bcf10','2d22f9f07552c183552bdbcbd24d9c70','4e790001bc1f1bad6b020b203cb19057','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','3d00fa58da21144f8214062dfe5bbdad','c42fd0630c8eb7c1aa804d56ec731d35','518b2462f5cde57f1ff2a15d7ee30faa','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
-INSERT INTO `usr_preferences` VALUES (7,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','announce_conn_type',0,'NULL','1900-01-01 00:00:01');
+INSERT INTO `usr_preferences` VALUES (7,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','announce_conn_type',0,'','1900-01-01 00:00:01');
 INSERT INTO `version` VALUES ('acc',5);
 INSERT INTO `version` VALUES ('acc_cdrs',2);
 INSERT INTO `version` VALUES ('active_watchers',12);
