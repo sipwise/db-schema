701c1f7aa5d40d0785c6f436dc4b2cba2efca9d2
Kirill Solomko <ksolomko@sipwise.com>
2024-07-22 10:40:12 +0200

    MT#60540 skip non-removable 15800 revision
    * 15800 revision contains additiona of `ngcp.release` column and
      this column is crucial during downgrades and cannot be removed,
      therefore, this revision is automatically skipped during downgrades
      as non-removable.
    
    Change-Id: Ic16119cf08cd239db089e5bb202ec5154bfb1713

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','29248791000f275a223cfc778e591bc8','3c6539c92d790db596d4d3ff901b3009','e8656ec396c5853394e50ba3f34b9b2f','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','99b242cceb9827ad236cfce8bfc6748f','960084f7f25d1cc2975405682b1b0798','b16000c8ef9c8b434e61102f5dfac931','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
