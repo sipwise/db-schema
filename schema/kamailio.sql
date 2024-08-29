397da1d5a04b7ca3b5209fb0fe528c6249d39152
Kirill Solomko <ksolomko@sipwise.com>
2024-08-29 15:33:27 +0200

    MT#60817 fix sync_ro_db() 'proxy' node role check
    * 'proxy' role is not correctly checked not only that it does
      not exists in the config.node_roles object but if it exists
      it is checked that it's True/False, to correctly skip sync_ro_db()
      if it's False
    
    Change-Id: I03061362c508e474782cf98e81020b067b4ff583

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','b8fd5762d9374bf4c41d3417b5a5ccfa','62ca8540d769d7f691d0ef0cd4b8a9ca','63586286dbbe66427f90fae6f913557e','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','1fc7ae8a38035899c4140a18d7c58a40','dc287379a731ffb01ad7577b7793bed8','bdc22ef2220214b5bd03fd3fb17d494a','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
