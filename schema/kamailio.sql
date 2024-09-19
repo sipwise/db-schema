ceb7032ba21aa1b32c5e4bef5203bbd867d3c8ae
Richard Fuchs <rfuchs@sipwise.com>
2024-09-19 10:13:44 -0400

    MT#53790 pushd: change `gcm` to `fcm`
    Support for the new HTTPS v1 protocol. GCM doesn't exist any more.
    
    (cherry picked from commit d75b113db8e9f667b7917782aa5e6bde30f630a1)
    (cherry picked from commit 09925b25cc341d9bde12e90402724216b51fd8b4)
    
    Change-Id: I7017e5474ef699a2dcaf6d4d7f815193a2819685

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -536,7 +536,7 @@ CREATE TABLE `members` (
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mobile_push_registrations` (
   `reg_id` varbinary(3072) NOT NULL,
-  `type` enum('gcm','apns') NOT NULL,
+  `type` enum('apns','fcm') NOT NULL,
   `subscriber_id` int(10) unsigned NOT NULL,
   `device_id` varbinary(255) NOT NULL,
   `apns_type` varchar(255) NOT NULL DEFAULT '',
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','2c10d21a4ff0de5f9f3da47adbac0a2d','8b8d121bfeac51e3b51091ec93a5f51e','d830973175ae4b7f6133244c37517757','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a2db519d9cf3d1a4062224c2bd1e60a1','84d1a0b6ddfb32b21ad112dd39d58538','a6796b4d1e7ae4c42ae5a4f48d02d937','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
