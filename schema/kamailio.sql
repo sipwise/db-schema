495c8606236aade5e5d12ef78bf8c4894e7aaf69
Kirill Solomko <ksolomko@sipwise.com>
2024-10-01 19:22:16 +0200

    MT#61041 improve RO db sync master node and upgrade
    * new command line boolean option --ngcp-upgrade that enables
      ngcp upgrade related logic (particularily sync_ro_db())
      if not specified - NGCP_UPGRADE env var is checked to
      auto enable it
    * sync_ro_db(): rework master node detection:
      - preferred master node selection is now only performed when
        ngcp_upgrade is enabled
      - fix and improve node side detection by either using regexp
        or checking if the name is 'sp1' or 'sp2'
        this logic is important because during the upgrade the sync must
        be done from the db node of the same side, as otherwise old database
        version and might be synced.
      - mysql database is no longer synchronised to 3308 (it's already
        removed from replication)
    
    (cherry picked from commit e5a4c75f97d350c5eee68e2c69b07200c3752241)
    (cherry picked from commit 4d014bcdff48e80ff5161aa21c9bcbe14dbdb32e)
    
    Change-Id: I151c725058627ec409b65a9e889a81a08f3a9129

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a2db519d9cf3d1a4062224c2bd1e60a1','84d1a0b6ddfb32b21ad112dd39d58538','a6796b4d1e7ae4c42ae5a4f48d02d937','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','f8e7b90a59669a9a7ae9fdad5dc1ee62','ffec67358f3ca9d1b1838a826bf32a61','a250420fad1813921e0c2041b32b0637','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
