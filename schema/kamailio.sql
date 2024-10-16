94ab2b6e35fabf5626f050c4cc759497112f118c
Donat Zenichev <dzenichev@sipwise.com>
2024-10-16 10:32:30 +0200

    MT#61254 preference: AA mixed mode of choice/dial ext. handling
    Add a preference, which introduces a mixed mode of handling AA
    choices and extensions dialing via AA menu.
    
    Can be enabled or disabled (default).
    If enabled then instant extensions dialing mode is used,
    otherwise the usual one (which requires dialing through
    the *asterisk sign).
    
    Can be applied at the following levels:
    - domain (has always default value 'disabled')
    - profile (inherits from domain if doesn't have own value)
    - subscriber (inherits from domain if doesn't have own value)
    
    Change-Id: Ibf1d08268a9b72d2524881951700776fdc9ec215

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -315,7 +315,7 @@ CREATE TABLE `dom_preferences` (
   PRIMARY KEY (`id`),
   KEY `ua_idx` (`uuid`,`attribute`),
   KEY `uda_idx` (`username`,`domain`,`attribute`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -1160,8 +1160,9 @@ INSERT INTO `dom_preferences` VALUES (28,'','0','voip.sipwise.local','mobile_pus
 INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_member_mode',0,'ring','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
+INSERT INTO `dom_preferences` VALUES (32,'','0','voip.sipwise.local','aa_extensions_dial_mode',0,'disabled','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','d3fbce02642e9967a1334f8d8d2a1a9b','b9ec4d0ebf8099f57c0c42599cab267d','db7585aa503ae4917b5167445693defe','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','2b9f56a4bed11f41ad987b9749c86205','f147d841857c884b7175934863c6857b','ed676bec6ac29c98c45497283cd7b9e7','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
