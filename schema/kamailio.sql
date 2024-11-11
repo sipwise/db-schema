acdf9d7c9707cdef67d7426fa199f6ac1c79aa83
Marco Capetta <mcapetta@sipwise.com>
2024-11-11 09:26:12 +0100

    MT#60677 ngcp-update-db-schema skip billing.journals for mysqld2
    billing.journals replication to 3308 is now ignored
    because it is not in use on proxies
    
    (cherry picked from commit 383f7523b178cfdefac5bb175396fd7906637888)
    
    Change-Id: I28100d7bb824f742378684010ce29e7794ccf875

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','b888e0b00a583c5ee3524c7cb96e823c','785b85ebd49728b26fd50a23974aa705','17e85b37067904244a44dc9fdf824120','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','e09874f1cb36bd371aef23a1716fa3a5','e891a785a5a2be01712b6d5a0cef62b5','9dd1d99104818a0042a592a3ad12b387','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
