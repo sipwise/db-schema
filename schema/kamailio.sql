1e9362c1ca8a7131071a16fb4be59b54715e13a0
Kirill Solomko <ksolomko@sipwise.com>
2024-11-12 15:49:30 +0100

    MT#61409 fix space when enlisting sync_ro_dbs
    (cherry picked from commit a9b6424088b9db552904cd86c3ebc7c310818e93)
    
    (cherry picked from commit 46262f1fccdb7a41ab4710f13477177c4e41aa20)
    
    Change-Id: Idfefe86479ee5462b687de2821c5ae3ef665b63f

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a89e8f21bde8de5862d4fd9c1c698cfd','af7979a316ffc964c4fe72a09e8cff50','4a227b75a50dfc81d5ceb224d50128a8','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','b44d3ff3caa9e8f7a4781bce28bf3791','abffc14933cc37ead5526faee24669e3','8ab47d5e106162a6caacd699b0c87f3c','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
