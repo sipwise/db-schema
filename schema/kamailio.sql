c32487c4b21177751b1098b383b283627ea1940b
Kirill Solomko <ksolomko@sipwise.com>
2024-11-13 15:34:35 +0100

    MT#61351 change 15783.up to not_replicated
    * reason for this change is because the related
      changes in the kamailio table get replicated to the
      active node during the upgrade and that breaks outbound peer
      provisioning / selection by running kamailio there
    
    (cherry picked from commit c332fc9d9f0b88aba60f141f88528654a7972439)
    
    Change-Id: I69416b5737cf05695cbfd1c2e4544faa8b1a2ab0

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1070,7 +1070,7 @@ INSERT INTO `dom_preferences` VALUES (27,'','0','voip.sipwise.local','play_annou
 INSERT INTO `dom_preferences` VALUES (28,'','0','voip.sipwise.local','mobile_push_enable',0,'never','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_member_mode',0,'ring','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','aad42f9fbebf73604d3cc23fb84944a7','b4d4f1a71b840498add705e7bd39aee0','5625b0bc85019f11157c778da26e63e1','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','faf8a1ce9d47b31f44e8c599645510a1','bd103de5041cdfba24bd48464bcf6b7a','39b1f039b2be665e98c37cc87c61cca4','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
