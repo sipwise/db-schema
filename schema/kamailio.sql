04682f4891799f91349b23f7da74dc99c88b649b
Kirill Solomko <ksolomko@sipwise.com>
2024-11-13 15:34:35 +0100

    MT#61351 change 15783.up to not_replicated
    * reason for this change is because the related
      changes in the kamailio table get replicated to the
      active node during the upgrade and that breaks outbound peer
      provisioning / selection by running kamailio there
    
    (cherry picked from commit c332fc9d9f0b88aba60f141f88528654a7972439)
    
    (cherry picked from commit 60ad7b8caf783f3d238f6355278e6e92e58a9feb)
    
    Change-Id: I69416b5737cf05695cbfd1c2e4544faa8b1a2ab0

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','b44d3ff3caa9e8f7a4781bce28bf3791','abffc14933cc37ead5526faee24669e3','8ab47d5e106162a6caacd699b0c87f3c','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','9cc84a28f9feb22412de4d94059bad1a','1a7962477117e7cfee95d119685a7043','36fda79f0696e6c72c110dea14c08575','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
