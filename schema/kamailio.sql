f24006d1d71c0d1bc4c82779ef34dbda0c9adf97
Kirill Solomko <ksolomko@sipwise.com>
2024-11-27 13:50:21 +0100

    MT#61626 PrettyTable debian/trixie compatibility
    * add_row() now uses list[any] instead of tuple[any]
      to be compatible with PrettyTable coming with debian/trixie
    
    Change-Id: I98cf5db91c6f63e13577f29ce437046898c5ba1c

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1162,7 +1162,7 @@ INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_c
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (32,'','0','voip.sipwise.local','aa_extensions_dial_mode',0,'disabled','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','a7d00a5cecf23792a10387d39aefa05a','2de4a9163615844fbc449358896dc2af','5bcbe622812e9ee74cb2d650d71d9883','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','087b57065497a020ce2663b2b3d86b45','22d6963490e97912c42a026d53c38e18','3b9bb86639286cf87e486cece58527bb','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
