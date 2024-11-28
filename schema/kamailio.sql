d08732ee81c262f7ca4abaa2cc03ca734731a643
Sipwise Jenkins Builder <jenkins@sipwise.com>
2024-11-28 14:55:29 +0000

    MT#61408 Don't fail if there is no revisions to delete
    This is rollback case if during the upgrade there is no
    not_replicated sql scripts.
    In 1st run of 'ngcp-update-db-schema --downgrade-release' deletes all
    the records will be deleted on sp1. So the next run on sp2 finds no
    records to delete and it's ok.
    So don't fail on this case.
    
    (cherry picked from commit 3143cc8e6042160eae97f3e126c261e9db07f0a6)
    Change-Id: Ic0e58914e2a42af7cf4e90918f12366dfef7312d

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','e09874f1cb36bd371aef23a1716fa3a5','e891a785a5a2be01712b6d5a0cef62b5','9dd1d99104818a0042a592a3ad12b387','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','13e30684709a905864f8dd801131f680','7612940ef79f8277f4c034a5d9fc2974','332bc1794114009c6eb253857f82c7ff','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
