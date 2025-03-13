1571e0396cf7f9e2909cf65188fae3a96ad42266
Kirill Solomko <ksolomko@sipwise.com>
2025-03-13 13:45:20 +0100

    MT#62349 remove triggers from 15783_not_replicated.up
    * triggers are removed from the not replicated script because
      they create an issue if this script is applied on the other
      node after 15791.up and 15797.up and revert the triggrs to
      inintended state.
    
    (cherry picked from commit ed514b8312862cc4a5984f7b84ca0e893e182f05)
    
    (cherry picked from commit 23d0482e0ecfb94ae48bcea2363600225bff3a0f)
    
    Change-Id: Iac830c24d0d51d693e6dbe76cea3766feebf7dd8

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1162,7 +1162,7 @@ INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_c
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (32,'','0','voip.sipwise.local','aa_extensions_dial_mode',0,'disabled','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','92c00e0c9367d22b08923fd16f3b8d29','aafc12cc20a6dc9b3b66e27893018d5d','190d59524699ee708eead69818e3212f','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','35189bc3652ed41ecd05f52a5caf83a3','8f4e154b495428adc8e8d691ffeb9608','1e4385f68274c47274bded4490f739ba','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
