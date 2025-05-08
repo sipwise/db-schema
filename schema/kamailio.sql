dc0c24668979c857a0bcf4cb1226711c4c9fdbb1
Kirill Solomko <ksolomko@sipwise.com>
2025-05-08 13:24:39 +0200

    MT#59836 fix voip_preferences_enum preference_id key
    base/8629.up introduced voip_preferences_enum with auto
    named foreign key as
    "FOREIGN KEY (preference_id) REFERENCES voip_preferences (id)"
    which auto created a missing index as
    `preference_id` (`preference_id`).
    
    diff/15787.up re-created the foreign key as named
    "ADD CONSTRAINT `pref_id_fk` FOREIGN KEY (`preference_id`) ... "
    which normally should cause the auto created `preference_id` key
    to be renamed to `pref_id_fk`.
    
    but customers who had their database re-synced, had that index
    turned from "auto created" to "permanent" and therefore, causing
    diff/15787.up to keep the original index name as `preference_id`.
    that in turn, caused ngcp-mysql-compare-dbs to report db inconsistencies
    as the default schema/.sql files are built with `pref_id_fk`.
    
    the current fix addresses the index issue by dropping it as either
    `preference_id` or `pref_id_fk` and creating it as a named index
    `pref_id_idx`, so that it wont be affected by the issue anymore.
    
    (cherry picked from commit 49f3d915eb91de246675ed01ce280b9adf7ce7b3)
    
    Change-Id: I4c777902251f915a1b07e92744d78e81087d607c

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1163,7 +1163,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','616ef1818cecde554b10851c6eda63e0','290f27cb93ecee46c38181683acddf12','ddb0751433cd7a6891276c56c7ae5040','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','10781afbbab28af0bc79b27cd9fdd25f','0777e8cdcfcc58422b916413b55a42fa','4d1d2ea22597756230b59ea935c704b8','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
