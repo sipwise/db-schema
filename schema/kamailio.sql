306456335517bb22ac4dddd773137b4832a94308
Kirill Solomko <ksolomko@sipwise.com>
2025-10-23 09:13:31 +0200

    MT#63753 fix fetch post_set_revisions for non-db nodes
    (cherry picked from commit dc2a069afdd82227ca19d0d88582b7179fcad809)
    
    (cherry picked from commit b644a5eb61eb3d10daa67880ed7fe2db915fe8f3)
    
    Change-Id: I1a500152d37549c4d09241cdf09a34327e4e61ab

--- schema/kamailio.sql
+++ schema/kamailio.sql
@@ -1161,7 +1161,7 @@ INSERT INTO `dom_preferences` VALUES (29,'','0','voip.sipwise.local','busy_hg_me
 INSERT INTO `dom_preferences` VALUES (30,'','0','voip.sipwise.local','announce_conn_type',0,'early','1900-01-01 00:00:01');
 INSERT INTO `dom_preferences` VALUES (31,'','0','voip.sipwise.local','record_call',0,'0','1900-01-01 00:00:01');
 INSERT INTO `domain` VALUES (1,'voip.sipwise.local','1900-01-01 00:00:01',NULL);
-INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','55e87e7dabb1d4a607d895727afa7c87','f5503892e8b8bf38c6d0d03f268726df','742c6b297f0965e636fc2658bb28105f','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
+INSERT INTO `subscriber` VALUES (1,'no_such_number','voip.sipwise.local','5cc56b031b4cc9ae7663c053cd406b4a','736088a1b9a1817aad9136cf34123111','63689f5cd377833eda0e1863b2e200c9','9bcb88b6-541a-43da-8fdc-816f5557ff93','','0000-00-00 00:00:00');
 INSERT INTO `usr_preferences` VALUES (1,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','cloud_pbx_hunt_policy',0,'none','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (5,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','emergency_location_format',0,'cirpack','1900-01-01 00:00:01');
 INSERT INTO `usr_preferences` VALUES (6,'9bcb88b6-541a-43da-8fdc-816f5557ff93','no_such_number','voip.sipwise.local','play_announce_before_recording',0,'never','1900-01-01 00:00:01');
